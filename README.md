99d017ec97061581f23ece02dcba3e95cf3a4b6c
CohleM <manish981194@gmail.com>
2020-08-08 14:00:21 +0545

    CartNumber Functioning fine

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-
 ## Under Construction
+
+###Run Frontend
+
+<yourDirectory\nepalimerchandise>npm start
+
+###Run Backend
+
+<yourDirectory\nepalimerchandise>cd backend
+
+<yourDirectory\nepalimerchandise>nodemon server
+
+###Extras
+Download Redux DevTools Chrome Extension
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -3,62 +3,67 @@ import { Link } from "react-router-dom";
 //import {  Badge } from "antd";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import { Badge } from "antd";
-export default class Navbar extends Component {
-	render() {
-		return (
-			<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-				<Link to="/" className="navbar-brand">
-					NepaliMerchandise
-				</Link>
-				<div className="collpase navbar-collapse">
-					<ul className="navbar-nav mr-auto">
-						<li className="navbar-item">
-							<Link to="/" className="nav-link">
-								Home
-							</Link>
-						</li>
-						<li className="navbar-item">
-							<Link to="/register" className="nav-link">
-								register
-							</Link>
-						</li>
-						<li className="navbar-item">
-							<Link to="/user" className="nav-link">
-								Create User
-							</Link>
-						</li>
-						<li className="navbar-item">
-							<Link exact to="/product/upload" className="nav-link">
-								uploadproduct
-							</Link>
-						</li>
+import { useSelector } from "react-redux";
+export default function Navbar() {
+	const prodCount = useSelector((state) => state.auth.user.cart);
 
-						<li className="navbar-item">
-							<Link exact to="/users/logout" className="nav-link">
-								logout
-							</Link>
-						</li>
+	//console.log(prodCount, prodCount.length);
+	let count = 0;
+	if (prodCount && prodCount.length >= 1) count = prodCount.length;
 
-						<li className="navbar-item">
-							<Link exact to="/users/login" className="nav-link">
-								Login
-							</Link>
-						</li>
+	return (
+		<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+			<Link to="/" className="navbar-brand">
+				NepaliMerchandise
+			</Link>
+			<div className="collpase navbar-collapse">
+				<ul className="navbar-nav mr-auto">
+					<li className="navbar-item">
+						<Link to="/" className="nav-link">
+							Home
+						</Link>
+					</li>
+					<li className="navbar-item">
+						<Link to="/register" className="nav-link">
+							register
+						</Link>
+					</li>
+					<li className="navbar-item">
+						<Link to="/user" className="nav-link">
+							Create User
+						</Link>
+					</li>
+					<li className="navbar-item">
+						<Link exact to="/product/upload" className="nav-link">
+							uploadproduct
+						</Link>
+					</li>
 
-						<li className="navbar-item">
-							<Link exact to="/users/cartPage" className="nav-link">
-								{/* <Icon
+					<li className="navbar-item">
+						<Link exact to="/users/logout" className="nav-link">
+							logout
+						</Link>
+					</li>
+
+					<li className="navbar-item">
+						<Link exact to="/users/login" className="nav-link">
+							Login
+						</Link>
+					</li>
+
+					<li className="navbar-item">
+						<Link exact to="/users/cartPage" className="nav-link">
+							{/* <Icon
 									type="shopping-cart"
 									style={{ fontSize: 30, marginBottom: 4 }}
 								/> */}
-								<Badge count={1} style={{}}>
-									<ShoppingCartOutlined style={{ fontSize: "20px" }} />
-								</Badge>
-							</Link>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		);
-	}
+							<Badge count={count} style={{}}>
+								<ShoppingCartOutlined style={{ fontSize: "20px" }} />
+							</Badge>
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 }
